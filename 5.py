f270c67f729f0a7a9935aefd3ea33c41e5784284
FedorMarch <fedorofmarch@gmail.com>
2021-07-20 15:51:07 +0300

    Д/з к занятию 6. Присылаю во вторник, но до 7 занятия (которое перенесли), так что, надеюсь, что зачтете как сданное вовремя :)

--- 5.py
+++ 5.py
@@ -1,6 +1,31 @@
-from functools import reduce
+class Stationery():
+    def __init__(self, title):
+        self.title = title
 
-my_list = [el for el in range(100, 1001) if el % 2 == 0]
-def multiply(el1, el2):
-    return el1 * el2
-print(reduce(multiply, my_list))
+    def draw(self):
+        print('Запуск отрисовки.')
+
+class Pen(Stationery):
+    def draw(self):
+        print(f'{self.title} проводит тонкую синию линию')
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f'{self.title} провел тонкую серую линию')
+
+class Handle(Stationery):
+    def __init__(self, title, color):
+        Stationery.__init__(self, title)
+        self.color = color
+
+    def draw(self):
+        print(f'{self.title} провел толстую линию цвета "{self.color}"')
+
+pen = Pen('Ручка')
+pen.draw()
+
+pencil = Pencil('Простой карандаш')
+pencil.draw()
+
+handle = Handle('Маркер', 'красный')
+handle.draw()
